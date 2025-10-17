c6f6bb416e32b92e17b43df9caf78ef815431c65
drzo1dberg <nunesjacobs@insicuri.net>
2025-10-17 22:07:46 +0200

    personal setup

--- init.lua
+++ init.lua
@@ -5,22 +5,13 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 require("custom.keymaps")
 require("custom.options")
--- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 -- [[ Setting options ]]
 -- See `:help vim.o`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
-
--- Make line numbers default
 vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
-
+vim.o.relativenumber = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = "a"
-
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
